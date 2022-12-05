70438f749d54bc06be280e7ccd90ddeb7ff3d497
gnauh <evanacademics@gmail.com>
2022-12-05 00:42:36 -0500

    re-wrote a bit

--- 2022/day3/day_3.go
+++ 2022/day3/day_3.go
@@ -6,21 +6,15 @@ import (
 	"log"
 	"os"
 	"strings"
+	"unicode"
 )
 
-var alpha = make(map[string]int)
-
 func sim(arr []string, size int) int {
-	sim_map := make(map[rune]int)
 	keys := ""
-
-	for i, item := range arr {
+	for _, item := range arr {
 		for _, ch := range item {
-			if sim_map[ch] == 0 {
+			if !strings.ContainsAny(keys, string(ch)) {
 				keys += string(ch)
-			}
-			if sim_map[ch] == i {
-				sim_map[ch]++
 			}
 		}
 	}
@@ -32,12 +26,18 @@ func sim(arr []string, size int) int {
 			}
 		}
 		if found == true {
-			//fmt.Println(alpha[string(key)])
-			return alpha[string(key)]
+			return alpha(key)
 		}
 	}
-
 	return 0
+}
+
+func alpha(ch rune) int {
+	if unicode.IsUpper(ch) {
+		return (int(ch) - 65) + 27
+	} else {
+		return (int(ch) - 97) + 1
+	}
 }
 
 func main() {
@@ -47,34 +47,26 @@ func main() {
 	}
 	defer file.Close()
 
-	for i := 0; i < 26; i++ {
-		alpha[string(rune(i+97))] = 1 + i
-		alpha[string(rune(i+65))] = 27 + i
-	}
-
 	scanner := bufio.NewScanner(file)
 	three_sack := make([]string, 3)
 	var p1, p2, rep = 0, 0, 0
 
 	for scanner.Scan() {
 		sack := make([]string, 2)
-
 		text := scanner.Text()
+
 		sack[0] = text[0 : len(text)/2]
 		sack[1] = text[len(text)/2:]
+		p1 += sim(sack, 2)
 
-		// 97 (lower)
 		three_sack[rep] = text
 		rep++
-		p1 += sim(sack, 2)
+
 		if (rep)%3 == 0 {
 			p2 += sim(three_sack, 3)
 			three_sack = three_sack[:3]
 			rep = 0
-
 		}
-
 	}
 	fmt.Printf("pt1: %d\npt2: %d", p1, p2)
-
 }
